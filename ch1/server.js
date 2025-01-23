149fc2a78204d2fec55563287e71002477aa7f65
Bruno Arakaki <brn.arakaki@gmail.com>
2025-01-23 12:49:40 +0000

    Handle message correctly for ws v8+

--- ch1/server.js
+++ ch1/server.js
@@ -2,7 +2,8 @@ var WebSocketServer = require('ws').Server,
 	wss = new WebSocketServer({ port: 8181 });
 wss.on('connection', function(ws) {
 	console.log('client.connected');
-	ws.on('message', function(message) {
+	ws.on('message', function message(data, isBinary) {
+		const message = isBinary ? data : data.toString();
 		console.log(message);
 	});
 });
